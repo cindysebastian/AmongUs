0c7247fde356d8f1b141359a943191639921b749
Cindy Sebastian <cindy.sebastian@gmx.net>
2024-03-25 10:25:12 +0100

    springboot initializer

---
+++ src/main/java/team5/amongus/AmongusApplication.java
@@ -0,0 +1,13 @@
+package team5.amongus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmongusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmongusApplication.class, args);
+	}
+
+}
