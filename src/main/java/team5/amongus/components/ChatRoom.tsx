8db7652a784369108fb68cf14ed6732b1f8dd96d
Cindy Sebastian <cindy.sebastian@gmx.net>
2024-04-09 20:17:09 +0200

    the tiniest design change ever

--- src/main/java/team5/amongus/components/ChatRoom.tsx
+++ src/main/java/team5/amongus/components/ChatRoom.tsx
@@ -4,8 +4,8 @@ const ChatRoom = ({messages}) => {
     return (
         <div>
             {messages.slice(0).reverse().map((message, index) => (
-                <div key={index}>
-                    <span>{message.sender}:</span>
+                <div key={index} style={{color: 'white'}}>
+                    <span>{message.sender}: </span>
                     <span>{message.content}</span>
                 </div>
             ))}
