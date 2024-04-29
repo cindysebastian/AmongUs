5fde821dd707e0083f61a5a20b85007a39978584
Büsra <bay6164@students.fhv.at>
2024-04-29 13:28:33 +0200

    everyone is in the game

--- src/main/java/team5/amongus/components/SpaceShip.tsx
+++ src/main/java/team5/amongus/components/SpaceShip.tsx
@@ -1,14 +1,28 @@
-import React, { useState, useEffect, useRef } from 'react';
-import SockJS from 'sockjs-client';
-import Stomp from 'stompjs';
+import React from 'react';
+import Player from './interfaces/Player'; // Import the Player interface
+import PlayerSprite from './PlayerSprite';
 import styles from '../../amongus/spaceship.module.css';
 
-const SpaceShip = ({ players }) => {
+interface Props {
+  players: Record<string, Player>;
+}
+
+const SpaceShip: React.FC<Props> = ({ players }) => {
   return (
     <div>
-      <div className={styles.gifBackground}></div> {/* GIF background */}
+      <div className={styles.gifBackground}></div>
       <div className={styles.spaceShipBackground}>
-        {/* Your spaceship content goes here */}
+        {/* Render players */}
+        {Object.values(players).map(player => (
+          <div key={player.name} style={{ position: 'absolute', top: player.position.y, left: player.position.x }}>
+            {/* Render each player's sprite */}
+            <PlayerSprite
+              player={player}
+              facing={player.facing !== undefined ? player.facing : 'RIGHT'}
+              isMoving={player.isMoving !== undefined ? player.isMoving : false}
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/main/java/team5/amongus/model/GameController.java
+++ src/main/java/team5/amongus/model/GameController.java
@@ -7,10 +7,9 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class GameController {
 
-    @MessageMapping("/startGame") // Define the endpoint for handling the start game message
-    @SendTo("/topic/gameStart")   // Broadcast the game start message to all clients subscribed to /topic/gameStart
+    @MessageMapping("/startGame")
+    @SendTo("/topic/gameStart")
     public String startGame() {
-        // Perform any necessary game initialization logic here
-        return "Game has started"; // You can customize the message if needed
+        return "Game has started";
     }
 }
