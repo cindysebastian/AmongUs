7f651e206fed11d8a36b2e1984cdd8390b2f0200
Büsra <bay6164@students.fhv.at>
2024-05-30 15:43:42 +0200

    justRoomCodeFrontend

--- src/main/java/team5/amongus/components/Lobby.tsx
+++ src/main/java/team5/amongus/components/Lobby.tsx
@@ -4,6 +4,7 @@ import Task from './interfaces/Interactible'
 import PlayerSprite from './PlayerSprite';
 import Space from './Space';
 import styles from '../styles/lobby.module.css';
+import { useParams } from 'react-router-dom';
 
 interface Props {
   inGamePlayers: Record<string, Player>;
@@ -29,21 +30,44 @@ const Lobby: React.FC<Props> = ({ inGamePlayers, onStartButtonClick, isHost}) =>
     }
   };
 
+  function copyToClipboard(text: string, element: HTMLElement) {
+    navigator.clipboard.writeText(text).then(() => {
+      console.log(`Copied to clipboard: ${text}`);
+      showTooltip(element);
+    }, (err) => {
+      console.error('Could not copy text: ', err);
+    });
+  }
+
+  function showTooltip(element: HTMLElement) {
+    const tooltip = document.createElement('div');
+    tooltip.className = 'tooltip';
+    tooltip.textContent = 'Copied!';
+    element.appendChild(tooltip);
+    setTimeout(() => {
+      tooltip.classList.add('show-tooltip');
+      setTimeout(() => {
+        tooltip.classList.remove('show-tooltip');
+        setTimeout(() => {
+          element.removeChild(tooltip);
+        }, 300);
+      }, 2000);
+    }, 10);
+  }
+
   return (
     <div style={{ position: 'relative' }}>
       <div className={styles.lobbyBackground}></div>
       <Space />
-      <div className={styles.roomCode}>
-        Room Code: {roomCode}
-      </div>
-      <div className={styles.playerCountContainer}>
+      <div className={styles.gameInfo}>
         <img src="src/main/resources/playerCountIcon.png" alt="Among Us Icon" className={styles.playerCountIcon} />
         <div className={styles.playerCount}>{playerCount}</div>
         {isHost && (
-          <div className={styles.startButtonContainer} onClick={handleStartButtonClick}>
+          <div className={styles.startButtonIcon} onClick={handleStartButtonClick}>
             <img src="src/main/resources/startButtonIcon.png" alt="Start Button Icon" className={`${styles.startButtonIcon} ${isStartButtonClicked ? styles.clicked : ''}`} />
           </div>
         )}
+        <div className={styles.gameCode} onClick={(e) => copyToClipboard(roomCode, e.currentTarget)}>CODE: {roomCode}</div>
       </div>
       {Object.values(inGamePlayers).map(player => {
         const isMoving = player.isMoving !== undefined ? player.isMoving : false;
