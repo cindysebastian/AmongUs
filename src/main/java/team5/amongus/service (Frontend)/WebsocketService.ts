a9371fe96adf455811a30bb02d20081c04b06d8a
denisemueller02 <146842156+denisemueller02@users.noreply.github.com>
2024-06-11 16:54:57 +0200

    Ip

--- src/main/java/team5/amongus/service (Frontend)/WebsocketService.ts
+++ src/main/java/team5/amongus/service (Frontend)/WebsocketService.ts
@@ -9,7 +9,7 @@ import { PlayersMap } from '../components/interfaces/Player';
 export const connectWebSocket = (setStompClient) => {
   // Disable Stomp.js logging
 
-  const socket = new SockJS('http://localhost:8080/ws');
+  const socket = new SockJS('http://10.0.40.165:8080/ws');
   const stomp = Stomp.over(socket);
   stomp.debug = null;
 
